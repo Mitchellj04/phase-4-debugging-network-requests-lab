ac63ea005de13f2d367eef8c9cdb938047fb876f
JustinMitchell <mitchelljm04@gmail.com>
2022-12-09 09:51:52 -0500

    Assignment Completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,5 @@
 class ToysController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :toy_create_error
   wrap_parameters format: []
 
   def index
@@ -7,13 +8,14 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy = Toy.find(params[:id])
+    toy.update(likes: toy.likes + 1)
+    render json: toy
   end
 
   def destroy
@@ -23,6 +25,10 @@ class ToysController < ApplicationController
   end
 
   private
+
+  def toy_create_error invalid
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
   
   def toy_params
     params.permit(:name, :image, :likes)
